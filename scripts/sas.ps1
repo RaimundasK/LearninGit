6aacd4521a094cc8562e7cd409f54e3db33011a6
RaimundasK <91466750+RaimundasK@users.noreply.github.com>
2025-01-10 10:10:29 +0200

    Update sas.ps1
    View code in codespace.

--- scripts/sas.ps1
+++ scripts/sas.ps1
@@ -1,4 +1,4 @@
-## Script to generate SAS token for Azure Storage account
+## Script to generate SAS token for Azure Storage account by RK
 
 using namespace System.Net
 using namespace Microsoft.WindowsAzure.Storage.Blob
